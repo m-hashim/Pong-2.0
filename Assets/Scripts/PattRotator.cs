afe8e5fae368747d37534b1c10093b12904e48cf
esteev <suryank@live.com>
2017-12-22 17:15:47 +0530

    Patt Rotator Beta Feature

--- Assets/Scripts/PattRotator.cs
+++ Assets/Scripts/PattRotator.cs
@@ -13,6 +13,11 @@ public class PattRotator : MonoBehaviour {
 	private const float ALPHA_CHANGE_TIME = 10f;
 	private const float MIN_ALPHA = 0.05f;
 	private const float MAX_ALPHA = 0.15f;
+	private const float MIN_SCALE = 0.9f;
+	private const float MAX_SCALE = 1.3f;
+	private const float SCALE_CHANGE_SPEED = 0.02f;
+	private const float SCALE_CHANGE_TIME = 10f;
+	private float SCALE_DIRECTION;
 
 	public Sprite[] patt;
 
@@ -20,6 +25,7 @@ public class PattRotator : MonoBehaviour {
 
 	void Start () {
 		ALPHA_DIRECTION = 1f;
+		SCALE_DIRECTION = 1f;
 		pattChanger ();
 	//	InvokeRepeating ("pattChanger", 0f, PATTERN_CHANGE_TIME);
 		InvokeRepeating ("directionChanger", 0f, ALPHA_CHANGE_TIME);
@@ -32,14 +38,20 @@ public class PattRotator : MonoBehaviour {
 		temp.a = Mathf.Min (MAX_ALPHA, temp.a + ALPHA_CHANGE_SPEED * Time.deltaTime * ALPHA_DIRECTION);
 		temp.a = Mathf.Max (MIN_ALPHA, temp.a);
 		GetComponent<Image> ().color = temp;
+		Vector3 tempScale = transform.localScale;
+		tempScale.x = Mathf.Min (MAX_SCALE, tempScale.x + SCALE_CHANGE_SPEED * Time.deltaTime * SCALE_DIRECTION);
+		tempScale.x = Mathf.Max (MIN_SCALE, tempScale.x);
+		tempScale.y = tempScale.x;
+		transform.localScale = tempScale;
 	}
 
 	void directionChanger()
 	{
 		ALPHA_DIRECTION *= -1;
+		SCALE_DIRECTION *= -1;
 	}
 
-	void pattChanger()
+	public void pattChanger()
 	{
 		GetComponent<Image> ().sprite = patt [Random.Range (0, SIZE)];
 	}
