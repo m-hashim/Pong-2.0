fcf5cea5fa3a030b90ef840849eafb15525ba78d
esteev <suryank@live.com>
2017-12-29 04:56:40 +0530

    Camera and score animations added.

--- Assets/Scripts/Gameplay/WallTouchAnim.cs
+++ Assets/Scripts/Gameplay/WallTouchAnim.cs
@@ -7,5 +7,10 @@ public class WallTouchAnim : MonoBehaviour {
 	void OnCollisionEnter(Collision col)
 	{
 		GetComponent<Animation> ().Play ();
+		if (gameObject.name.Contains ("East")) {
+			GameManager.Instance.player_Score.GetComponent<Animation> ().Play ();
+		}else if (gameObject.name.Contains ("West")) {
+			GameManager.Instance.AI_Score.GetComponent<Animation> ().Play ();
+		}
 	}
 }
